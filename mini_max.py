b244892744ec89f1dc87383bbabd24f21dc81b15
Mostafa Saad <83826642+Mostafa11412@users.noreply.github.com>
2023-05-19 11:15:33 +0300

    minimize player added to mini_max

--- mini_max.py
+++ mini_max.py
@@ -11,26 +11,37 @@ BLUE = 2
 
 
 
-def mini_max_fun(board , depth , alpha , beta , maximizing_player):
-    (game_board, game_end,player) = board.get_game_grid()
+def mini_max_fun(board , depth , alpha , beta , current_player):
+    (game_board, game_end,current_player) = board.get_game_grid()
     if depth == 0 or game_end:
-        return evaluate(board)
+        return evaluate(board, current_player)
 
 
-    if maximizing_player:
+    if current_player == RED:
         max_eval = float('-inf')
         for col in range(7):
             (valid , grid) = board.move(col,RED)
             if(not valid):
                 continue
-            eval = mini_max_fun(grid , depth-1 , alpha, beta,False)
+            eval = mini_max_fun(grid , depth-1 , alpha, beta,BLUE)
             max_eval = max(max_eval, eval)
             alpha = max(alpha, eval)
             if beta <= alpha:
                 break
-            return max_eval
+        return max_eval
 
-
+    else:
+        min_eval = float('inf')
+        for col in range(7):
+            (valid, grid) = board.move(col, BLUE)
+            if(not valid):
+                continue
+            eval = mini_max_fun(grid, depth-1, alpha, beta, RED)
+            min_eval = min(min_eval, eval)
+            beta = min(beta, eval)
+            if beta <= alpha:
+                break
+        return min_eval
 
 
 
